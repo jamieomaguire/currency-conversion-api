5723d2986d711bee23d510954a5ded31414d5e65
jamieomaguire <jamieolivermaguire@googlemail.com>
2016-10-15 16:11:29 +0100

    for loop now creates pushes countries to an array, which can be used later within the program

--- components/countryByCurrency.php
+++ components/countryByCurrency.php
@@ -9,9 +9,15 @@ $REQUEST = file_get_contents($ENDPOINT);
 // format the response as an array
 $responseArray = json_decode($REQUEST, true);
 
+// initialise array of countries
+$countriesList = [];
+
 // iterate through the array to get the names of countries
 for ($i = 0; $i < count($responseArray); $i++ ) {
-    echo '<pre>';
-    var_dump($responseArray[$i]["name"]);
-    echo '</pre>';
+    array_push($countriesList, $responseArray[$i]["name"]);
 }
+
+
+echo '<pre>';
+var_dump($countriesList);
+echo '</pre>';
