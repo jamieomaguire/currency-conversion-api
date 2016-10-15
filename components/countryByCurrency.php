2049229b663c46433197fc98f4bae1be84c5fada
jamieomaguire <jamieolivermaguire@googlemail.com>
2016-10-15 17:11:14 +0100

    wrapped the code to create a country list in a function and created an empty file for the xml generation script.

--- components/countryByCurrency.php
+++ components/countryByCurrency.php
@@ -1,10 +1,14 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Build the request uri
 $URI = 'https://restcountries.eu/rest/v1/currency/';
 
 // Any currency code can go here
-$CURRENCY = 'gbp';
+$CURRENCY = 'GBP';
 
 // Join the two components of the request
 $ENDPOINT = $URI . $CURRENCY;
@@ -13,16 +17,27 @@ $REQUEST = file_get_contents($ENDPOINT);
 // Format the response as an array
 $responseArray = json_decode($REQUEST, true);
 
-// Initialise array of countries
-$countriesList = [];
+// Create the country list in a function
+function createCountriesList($responseArray) {
 
-// Iterate through the array to get the names of countries
-for ($i = 0; $i < count($responseArray); $i++ ) {
-    array_push($countriesList, $responseArray[$i]["name"]);
+    // Initialise array of countries
+    $countriesList = [];
+
+    global $countriesStringList;
+
+    // Iterate through the array to get the names of countries
+    for ($i = 0; $i < count($responseArray); $i++ ) {
+        array_push($countriesList, $responseArray[$i]["name"]);
+    }
+
+    // Turn array into a string seperated by commas and spaces
+    $countriesStringList = implode(', ', $countriesList);
+
+    return $countriesStringList;
+
 }
 
-// Turn array into a string seperated by commas and spaces
-$countriesStringList = implode(', ', $countriesList);
+createCountriesList($responseArray);
 
 // Echo out the list, for luls
 echo '<p>' . $countriesStringList . '</p>';
