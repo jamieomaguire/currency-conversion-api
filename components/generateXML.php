da1bc7e0ae913ee98f2e9260452ae1bd4efe7e2e
jamieomaguire <jamieolivermaguire@googlemail.com>
2016-10-17 10:29:08 +0100

    now generates xml with locations

--- components/generateXML.php
+++ components/generateXML.php
@@ -2,12 +2,13 @@
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
+ini_set('max_execution_time', 600);
 error_reporting(E_ALL);
 
 include 'countryByCurrency.php';
 
 // Insert API key here
-$API_KEY = "";
+$API_KEY = "18947b8fdda74706b676b4ab92faa09d";
 $exchange_rate_url = 'https://openexchangerates.org/api/latest.json?app_id=';
 
 $json_data = file_get_contents($exchange_rate_url  . $API_KEY);
@@ -15,14 +16,15 @@ $data = json_decode($json_data, true);
 $rates = array_slice($data, 4);
 $xml = new SimpleXMLElement('<currencies/>');
 
+
 foreach($rates as $keys => $values) {
   foreach($values as $key => $value) {
-    // createList($key);
+    createList($key);
     $currency = $xml->addChild('currency');
     $currency->addAttribute('rate', $value);
     $currency->addAttribute('code', $key);
     $currency->addChild('name');
-    $currency->addChild('locations');
+    $currency->addChild('locations', $countriesResult);
   }
 }
 
